2f713af0b93882f9cbef75c06babcd2822f630e5
ChrUbuntu User <user@chromebox-005.(none)>
2013-03-11 18:31:37 -0700

    This is Exercise06: Dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,39 +1,20 @@
 from sys import argv
-
 script, filename = argv
+#import string
 
-# opens the file and makes it one string that keeps the new lines
-text = open(filename).read()
+def count_words(filename):
+    in_file = open(filename)
+    read_file = in_file.read().lower()
+    in_file.close()
+    replace_file = read_file.replace(".", " ").replace(",", " "). replace("?", " ")
+    word_list = replace_file.split()
 
-# splits the text string by the lines into a list of lines
-lineList = text.splitlines()
+    dictionary = {}          
+    for word in word_list:
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    print dictionary
 
-wordList = []
-
-for i in lineList:
-	words = i.split(' ')
-	for j in range(len(words)):
-		words[j] = (words[j].strip('?.*[]()1234567890,')).lower()
-	wordList += words
-# now we have a list, wordList of all the words in the file, converted to lowercase
-
-wordDict = {}
-
-"""
-# this is the same for loop to add keys and counts to the dictionary, just using the get method
-for word in wordList:
-	if wordDict.get(word, 0) == 0:
-		wordDict[word] = 1
-	else:
-		wordDict[word] += 1
-"""
-
-for word in wordList:
-	if word not in wordDict:
-		wordDict[word] = 1
-	else:
-		wordDict[word] += 1
-
-for key, value in sorted(wordDict.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s: %s" % (key, value)
-
+count_words(filename)
